05e1167722bbc48bccfffb7a21601790e6a2734a
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-10-04 14:51:08 +0300

    add a comment in the file

--- PRL/events_insert.php
+++ PRL/events_insert.php
@@ -76,6 +76,7 @@ if (isset($_POST['committee']) && !empty(trim($_POST['committee']))) {
     }
 }
 
+//this inclusion for the events notificatin file, to send events using firebase
 include_once '../android/eventNotification.php';
 
 //bellow is the code related to store the event in the db.
