91db880d5b11747a27bc4ff0e86c81d14e03b4fd
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-10-07 13:42:15 +0300

    include eventNotification test file

--- PRL/events_insert.php
+++ PRL/events_insert.php
@@ -77,7 +77,10 @@ if (isset($_POST['committee']) && !empty(trim($_POST['committee']))) {
 }
 
 //this inclusion for the events notificatin file, to send events using firebase
-include_once '../android/eventNotification.php';
+//include_once '../android/eventNotification.php';
+
+//this inclusion for notification test
+include_once '../android/eventNotification1.php';
 
 //bellow is the code related to store the event in the db.
 $event1 = new events();
