30a45458e5b6d2016746fdb61eaf303ca4214dad
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-01 15:03:22 +0200

    changed the events select statement to get the needed data from the events table

--- BLL/android/events.php
+++ BLL/android/events.php
@@ -5,7 +5,10 @@ include_once '../../DAL/my_db.php';
 class events extends my_db {
 
     function get_events() {
-        return $this->get_all_data("SELECT EVENTS.subject FROM `events`");
+        return $this->get_all_data("SELECT committees.committee_name, "
+                        . "events.subject, events.event_date, events.time "
+                        . "FROM committees, events "
+                        . "WHERE committees.committee_id = events.committee_id");
     }
 
 }
