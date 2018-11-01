533cf80adb1accb781816e5957340e3ca367fcb2
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-01 12:11:02 +0200

    test code for storing user token in the db

--- android/saveUserToken.php
+++ android/saveUserToken.php
@@ -1,9 +1,6 @@
 <?php
 
-include '../DAL/my_con.php';
+include_once '../BLL/android/user_token.php';
 
-$statement = $con->prepare("INSERT INTO `notify_user`(`notify_user_token`) VALUES (?)");
-
-$statement->bind_param("s", $_GET['token']);
-
-$statement->execute();
+$user_token = new user_token();
+$user_token->store_user_token($_GET['userToken']);
