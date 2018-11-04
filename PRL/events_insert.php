2becd6f2c2eb6ef62b5e2cb87e898d94763cf7a9
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-04 11:40:42 +0200

    i've included the eventNotification file in the event insert page to send notification for the android app when a new event inserted

--- PRL/events_insert.php
+++ PRL/events_insert.php
@@ -46,4 +46,8 @@ if (isset($_POST['committee']) && !empty(trim($_POST['committee']))) {//here i c
 $event1 = new events();
 $event1->insert_event($event_entity, $event_entity_name, $event_time, $event_appointment, $hall_id, $event_place, nl2br($_POST['subject']), $_POST['event_date'], $event_status, $_SESSION['directorate'], $_SESSION['user_id']);
 
+/* hree i'll call the eventsNotification file to send notification for the 
+  android app */
+include_once '../android/eventNotification.php';
+
 header('location: events_preview_current_future.php');
